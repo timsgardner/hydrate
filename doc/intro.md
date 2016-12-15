21fe253ef9664555b93c23c23ca3c1230fc1920c
timsgardner <timsgardner@gmail.com>
2016-12-14 22:09:52 -0500

    we're back!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to timsg.hydrate
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/timsg/hydrate_test.clj
@@ -0,0 +1,7 @@
+(ns timsg.hydrate-test
+  (:require [clojure.test :refer :all]
+            [timsg.hydrate :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
